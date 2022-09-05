d653ec60dfabfc1283c448e1f179b4102186abb1
GeeksMinister <nael_sps@hotmail.com>
2022-09-05 12:55:58 +0200

    Bootstrap popup modal for angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Employee } from './models/Employee';
 import { EmployeeService } from './services/employee-service';
+
+declare var window: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(private employeeService: EmployeeService) { }
+
+  formModal: any;
 
   title = 'Employees';
   employees: Employee[] = [];
@@ -17,9 +21,20 @@ export class AppComponent {
   ngOnInit() {
     this.employeeService.getEmployees()
     .subscribe((result: Employee[]) => (this.employees = result));
+
+    this.formModal = new window.bootstrap.Modal(
+      document.getElementById("exampleModal")
+    );
+
   }
   
+  openModal(){
+    this.formModal.show();
+  }
 
+  doSomething(){
+    this.formModal.hide();
+  }
   
 
 }
